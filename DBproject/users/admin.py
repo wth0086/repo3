e983e01cdfa1d010434622983f600818b61f70b3
Woo Tae Hee <wth0086@naver.com>
2020-11-12 15:47:46 +0900

    woo add users model&admin

--- DBproject/users/admin.py
+++ DBproject/users/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "gender",
+                    "birthdate",
+                    "country",
+                    "address",
+                    "language",
+                    "currency",
+                )
+            },
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+    )
+    
--- DBproject/users/models.py
+++ DBproject/users/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django_countries.fields import CountryField
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    country = CountryField()
+    address = models.CharField(max_length=140)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+
+
