7dd28367f218402d60b8841b8d46d0d60f6063bd
Woo Tae Hee <wth0086@naver.com>
2020-11-13 08:22:52 +0900

    git ignore add

--- DBproject/rooms/admin.py
+++ DBproject/rooms/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.BedType, models.Amenity)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    filedsets = (
+        (
+            "fields",
+            {
+                "fields": (
+                    "room_number",
+                    "room_type",
+                    "bed_type",
+                    "amenities",
+                    "room_state",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "room_number",
+        "room_type",
+        "bed_type",
+        "room_state",
+    )
+
+    filter_horizontal = ("amenities",)
--- DBproject/rooms/models.py
+++ DBproject/rooms/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Model Definition"""
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class BedType(AbstractItem):
+    class Meta:
+        verbose_name = "Bed Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Room(core_models.TimeStampedModel):
+    room_number = models.CharField(max_length=5)
+    room_type = models.ForeignKey(
+        RoomType, related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    bed_type = models.ForeignKey(
+        BedType, related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
+    room_state = models.BooleanField(default=True)
